42213f3d2a7a9a7441ab7c360306431e28bc242b
jesussssssd <125912684+jesussssssd@users.noreply.github.com>
2023-02-20 19:08:27 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+package cuentas;
+
+
+import cuentas.CCuenta;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Bienvenido
+ */
+
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
